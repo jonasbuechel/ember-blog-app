2430fa39e51b0fa63debaeaee2aed6f4bf58ba33
Jonas Büchel <jonas.buechel@gmail.com>
2016-06-03 12:59:57 +0200

    reset Storage Adapter & Serializer integration in app.js, because this is now in pods/application

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-import DS from 'ember-data';
 
 let App;
 
@@ -13,12 +12,6 @@ App = Ember.Application.extend({
   podModulePrefix: config.podModulePrefix,
   Resolver
 });
-
-//TODO: FIX THIS FOR EMBER 2.x!
-/*App.ApplicationSerializer = DS.LSSerializer.extend();
-App.ApplicationAdapter    = DS.LSAdapter.extend({
-    namespace: 'ember-blog'
-});*/
 
 loadInitializers(App, config.modulePrefix);
 
